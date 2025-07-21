0d98461a3eb085550a5b579e6d96e1bdc3019f4f
Jadonamite <jadonamite@gmail.com>
2025-07-21 20:50:17 +0100

    dashboard nav

---
+++ app/dashboardPage.js
@@ -0,0 +1,17 @@
+import Nav from "../components/nav/Nav";
+import Hero from "../components/Hero";
+import NFTGrid from "../components/NFTGrid";
+import ActionSection from "../components/ActionSection";
+import CollectionTable from "../components/CollectionTable";
+
+export default function Home() {
+   return (
+      <div className="min-h-screen bg-neutral">
+         <Nav />
+         <Hero />
+         <CollectionTable />
+         <NFTGrid />
+         <ActionSection />
+      </div>
+   );
+}
